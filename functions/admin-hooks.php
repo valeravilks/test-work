0cf4c8a58d70054618f18d5a4442c118557fe27b
Mark Atli <mark@dreamersofday.co>
2021-12-07 14:40:47 -0600

    color changes

--- functions/admin-hooks.php
+++ functions/admin-hooks.php
@@ -9,8 +9,8 @@
 add_filter('show_admin_bar', '__return_false');
 
 // Hide the ACF Fields Group menu item for the staging and production sites.
-// add_filter('acf/settings/show_admin', function() {
-//     $env = wp_get_environment_type();
-//     $hide_list = ['production', 'staging'];
-//     return !in_array( $env, $hide_list );
-// });
+add_filter('acf/settings/show_admin', function() {
+    $env = wp_get_environment_type();
+    $hide_list = ['production', 'staging'];
+    return !in_array( $env, $hide_list );
+});
