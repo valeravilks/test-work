75d089bf7c362e8fcb5ef5b3170540b647678858
Mark Atli <mark@dreamersofday.co>
2021-12-14 06:07:24 +0100

    clickable video

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -66,6 +66,7 @@ const app = {
 	floatingCta,
 	socialShare,
 	hamburger,
+	clickableVideo,
 	defaultSlideOut,
 	modal,
 	mapBlock,
--- src/patterns/03-organisms/custom/i360-clickable-video/i360-clickable-video.js
+++ src/patterns/03-organisms/custom/i360-clickable-video/i360-clickable-video.js
@@ -1,1 +1,3 @@
-export default () => {};
+export default () => {
+	//HAYK ADD MICRO MODAL CODE HERE. Check "src/patterns/02-molecules/modal" how to do
+};
