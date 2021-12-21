417de53fe3a0d9c8cd30b1f19244860c1c4ec6ca
Hayk Akunyan <haykak5@gmail.com>
2021-12-21 11:23:27 +0400

    Have done bug fixing for modal molecule and #73 task from i360 Trello Development Board.

--- src/patterns/02-molecules/modal/modal.js
+++ src/patterns/02-molecules/modal/modal.js
@@ -1,6 +1,27 @@
 import MicroModal from 'micromodal';
 
 export default () => {
+	let componentClass = 'm-card-simple-alt';
+
+	// Brings modal container above all DOM elems
+	$(`.${componentClass}--contains-modal`).on('click', function() {
+		$(this).addClass(`${componentClass}--contains-modal-active`);
+	});
+
+	// Modal out-click logic
+	$(document).on('click', function(e) {
+		if (
+			$(e.target).is(`.a-video`) === false &&
+			$(`.a-video`).has(e.target).length === 0 &&
+			$(e.target).is(`.m-modal__link`) === false &&
+			$(`.m-modal__link`).has(e.target).length === 0
+		) {
+			$(`.${componentClass}--contains-modal`).removeClass(
+				`${componentClass}--contains-modal-active`
+			);
+		}
+	});
+
 	MicroModal.init({
 		disableScroll: true, // [6]
 		awaitOpenAnimation: false, // [8]
